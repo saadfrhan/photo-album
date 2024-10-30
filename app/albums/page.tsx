e96f4125ca80210e0878efb864130e523fc320a0
saadfrhan <saadfarhan347@gmail.com>
2024-10-30 18:24:18 +0500

    update grid

--- app/albums/page.tsx
+++ app/albums/page.tsx
@@ -1,15 +1,17 @@
-import cloudinary from "cloudinary";
-import { AlbumCard } from "./album-card";
-import { Folder } from "@/types";
+import cloudinary from "cloudinary"
+
+import type { Folder } from "@/types"
+
+import { AlbumCard } from "./album-card"
 
 export const metadata = {
   title: "Albums",
-};
+}
 
 export default async function AlbumsPage() {
   const { folders } = (await cloudinary.v2.api.root_folders()) as {
-    folders: Folder[];
-  };
+    folders: Folder[]
+  }
 
   return (
     <section>
@@ -18,12 +20,12 @@ export default async function AlbumsPage() {
           <h1 className="text-4xl font-bold">Albums</h1>
         </div>
 
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-4 gap-4 max-[1000px]:grid-cols-2 max-[768px]:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4">
           {folders.map((folder) => (
             <AlbumCard key={folder.path} folder={folder} />
           ))}
         </div>
       </div>
     </section>
-  );
+  )
 }
